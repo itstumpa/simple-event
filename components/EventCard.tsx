20d09fc9f6e6bfcfe65620806b0805573081e3a4
tumpa <tumpadas3494@gmail.com>
2025-11-24 16:08:45 +0600

    mongoose

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -22,20 +22,19 @@ interface Props {
 const EventCard = ({ title, image, slug, location, date, time }: Props) => {
   return (
     <Link href={`/events/${slug}`} id="event-card">
-      <div className="poster">
-        <Image src={image} alt={title} width={410} height={300} />
-      </div>
+     
+        <Image src={image} alt={title} width={410} height={300} className="poster"/>
+      
       <div className="flex flex-row gap-2">
        <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
               <p>{location}</p>
       </div>
-      <div className="title">
-        <h4>{title}</h4>
-      </div>
+
+      <p className="title">{title}</p>
 
       <div className="datetime">
        <div>
-               <Image src="/icons/calender.svg" alt="date" width={14} height={14} />
+               <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
               <p>{date}</p>
        </div>
        <div>
